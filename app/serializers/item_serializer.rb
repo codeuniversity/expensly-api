e0c0ebce3dc535ff3161fb5b6bbf37de89f714b1
alexmorten <alexandrius.martinus@gmx.de>
2017-12-11 12:59:21 +0100

    serialize whole category in item

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,11 +1,18 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :price, :name, :category_name, :amount
-
+  attributes :id, :price, :name, :amount, :cost
+  belongs_to :category
   def name
     object.article.name
   end
+
+  def cost
+    object.price * object.amount
+  end
+
   def category_name
     object.article.category.name
   end
-
+  def category
+    object.article.category
+  end
 end
