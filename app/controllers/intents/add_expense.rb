0161047da8394512279056d5cab16c43d52c3874
alexmorten <alexandrius.martinus@gmx.de>
2017-12-12 12:30:05 +0100

    add transaction saver

--- app/controllers/intents/add_expense.rb
+++ app/controllers/intents/add_expense.rb
@@ -1,20 +1,29 @@
 module Intents
   class AddExpense < IntentHandler
-    def handle(slots, session_attributes={})
     def handle(current_user, slots, session_attributes={})
      @article = slots['article']['value']
       @amount = slots['amount']['value']
       slot_info = {article: @article, amount: @amount}
-      session_info = {article: session_attributes['article'], amount: session_attributes['amount']}
       ap slot_info
-      ap session_info
-      info = session_info.merge(slot_info.compact).compact
+      info = session_attributes.symbolize_keys.merge(slot_info.compact).compact
       ap info
-      if info[:article] && info[:amount]
-        ask("I am going to add #{info[:article]}, for #{info[:amount]} euros", info)
-      elsif !info[:article]
+      article = info[:article]
+      amount = info[:amount]
+      if article && amount
+        items = info[:items]&.map(&:symbolize_keys) || []
+        items.push({article: info[:article], amount: info[:amount]})
+        info[:items] = items
+        total = 0
+        ap info
+        items.each {|item| total += item[:amount].to_f}
+        # byebug
+        ap total
+        info[:article] = nil
+        info[:amount] = nil
+        ask("I am adding #{article}, for #{amount} euros. You have a total of #{total} in this transaction now.", info)
+      elsif !article
         ask('What did you buy?', info)
-      elsif !info[:amount]
+      elsif !amount
         ask('How much did you pay?', info)
       end
 
