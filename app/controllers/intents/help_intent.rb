51556b20d0d0f13aeefcd42bd98d8d4ac043c3e8
alexmorten <alexandrius.martinus@gmx.de>
2017-12-12 12:30:15 +0100

    improve help

--- app/controllers/intents/help_intent.rb
+++ app/controllers/intents/help_intent.rb
@@ -1,6 +1,7 @@
 module Intents
   class HelpIntent < IntentHandler
     def handle(current_user, slots, session_attributes)
+      answer ["You could say something like 'I bought a phone for 100 euros' or ask what you spent money on"].sample
     end
   end
 end
